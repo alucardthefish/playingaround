216644d3505aaa2287d9e16c9b9cdf51b422bc13
Sergio Ortiz Paz <sergiop18@gmail.com>
2023-11-19 17:37:43 -0500

    Add Angular UI
    Add UI in case want to experiment with user interface.

---
+++ playingaround-ui/src/app/jobs/jobs.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { JobsService } from "../jobs.service"
+
+@Component({
+  selector: 'app-jobs',
+  templateUrl: './jobs.component.html',
+  styleUrls: ['./jobs.component.css']
+})
+export class JobsComponent implements OnInit {
+
+  componentName: string = "Jobs"
+  jobs: any;
+
+  constructor(private jobsService: JobsService) { }
+
+  ngOnInit() {
+  	this.jobsService.getAllJobs().subscribe(
+  		res => { 
+  			this.jobs = res;
+  			console.log("jobs: ", this.jobs); 
+  		},
+  		error => console.log("error: ", error)
+  	);
+  }
+
+}
